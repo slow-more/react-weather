20e2a8cb8c4df051b7600c5a76a2943146ed583b
reactor <iwashidev@gmail.com>
2024-10-18 21:01:30 +0900

    add disp input text

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
+
 const Form = ()=>{
+    const [city, setCity] = useState("")
+
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名を入れてね"/>
+            <input type="text" name="city" onChange={e => setCity(e.target.value)} placeholder="都市名を入れてね"/>
+            {city}
             <button type="submit">天気情報を取得</button>
         </form>
     )
