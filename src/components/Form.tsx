fabe83b5ab06cc30816eb97e839cdc40e840dbc0
Seiya Takada <seiyatakada@nokkfjawwo.local>
2024-10-18 20:37:58 +0900

    make form dom

---
+++ src/components/Form.tsx
@@ -0,0 +1,9 @@
+const Form = ()=>{
+    return (
+        <form>
+            <input type="text" name="city" placeholder="都市名を入れてね"/>
+            <button type="submit">天気情報を取得</button>
+        </form>
+    )
+}
+export default Form;
