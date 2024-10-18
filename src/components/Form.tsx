86ea4f5d0670ea14f5b118851934d56477cf06ae
reactor <iwashidev@gmail.com>
2024-10-18 21:50:59 +0900

    apiのレスポンスを取得 #日本語と改行チェック

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
 
 const Form = ()=>{
-    const [city, setCity] = useState("")
+    const [city, setCity] = useState<String>()
 
+    const getWeather = (e:any)=>{
+        e.preventDefault()
+        fetch(`https://api.weatherapi.com/v1/current.json?key=446901a7634b495b9e5123329241810&q=London&aqi=no`)
+        .then(data => data.json())
+        .then(data => console.log(data))
+    }
+
+    
     return (
         <form>
             <input type="text" name="city" onChange={e => setCity(e.target.value)} placeholder="都市名を入れてね"/>
             {city}
-            <button type="submit">天気情報を取得</button>
+            <button type="submit" onClick={getWeather}>天気情報を取得</button>
         </form>
     )
 }
