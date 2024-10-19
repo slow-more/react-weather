554e95a6ac7c80d386b94eaa27b4504cc0b14fad
reactor <iwashidev@gmail.com>
2024-10-19 14:14:18 +0900

    apiデータから国名を表示

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,46 @@ import Form from './components/Form'
 import Result from './components/Result'
 
 const App = () => {
+
+  type WeatherDataType = {
+    country:string,
+    cityName:string,
+    temperature:string,
+    conditionText:string,
+    icon:string
+  }
+
   const [city, setCity] = useState<String>("")
+
+  const [weatherData, setWeatherData] = useState<WeatherDataType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  })
 
   const getWeather = (e:any)=>{
       e.preventDefault()
       fetch(`https://api.weatherapi.com/v1/current.json?key=446901a7634b495b9e5123329241810&q=London&aqi=no`)
-      .then(data => data.json())
-      .then(data => console.log(data))
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setWeatherData({
+          country: data.location.country,
+          cityName: data.location.region,
+          temperature: data.current.condition.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.location.lon
+        })
+      })
   }
 
   return (
     <div>
       <Title/>
       <Form setCity={setCity} getWeather={getWeather}/>
-      <Result/>
+      <Result weatherData={weatherData}/>
     </div>
   )
 }
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,7 +1,19 @@
-const Result = ()=>{
+// todo:App.tsxにも書かれている型宣言は悪くないか？
+type weatherDataProps = {
+    weatherData: {
+        country:string,
+        cityName:string,
+        temperature:string,
+        conditionText:string,
+        icon:string
+    }
+}
+
+const Result = (props:weatherDataProps)=>{
     
     return (
-        <h1>気象データ</h1>
+        <h1>気象データ{props.weatherData.country}</h1>
+        // <p>{weatherData.results.country}</p>
     )
 }
 
